056373d387e596e52a720421119883f48b5294dd
Jesse Alves <jesse.alves@tecsinapse.com.br>
2017-06-28 19:36:11 -0300

    v0.0.2

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Npm.depends({
 
 Package.describe({
   name: 'tecsinapse:keycloak-meteor',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Keycloak login integrated with Accounts',
   git: 'https://github.com/tecsinapse/tecsinapse-keycloak-meteor.git',
   documentation: 'README.md'
